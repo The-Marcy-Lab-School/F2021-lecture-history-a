20e1cc598eb625dbb7c4aed45eb3675f01a2f6f2
Ann <thuyanannduong@gmail.com>
2021-11-08 19:43:58 +0000

    review

---
+++ unit-2/review_2_debugging/README.md
@@ -0,0 +1,15 @@
+# JS Review
+
+
+## Review Topics
+- Spread Operator (...)
+- Rest Operator (...)
+- Destructuring 
+- Pass by Value vs Pass By Reference 
+
+
+## Debugging in JavaScript
+- [Debugging](https://en.wikipedia.org/wiki/Debugging) is the process of finding and resolving [bugs](https://en.wikipedia.org/wiki/Software_bug) in a program
+- [Paired Programming](https://en.wikipedia.org/wiki/Pair_programming)
+- [Rubber Duck Debugging](https://en.wikipedia.org/wiki/Rubber_duck_debugging)
+- `debugger` and Breakpoints
---
+++ unit-2/review_2_debugging/script.js
@@ -0,0 +1,69 @@
+// //Pass By Reference
+
+// function double(a){
+//     let b = a 
+//     b.number = a.number * 2
+//     b.string = `${a.number} is now a string`
+//     return b
+// }
+
+// let num = {number: 3}
+// let doubleNum = double(num)
+
+// console.log(num)        //what is the value of num?
+// console.log(doubleNum)  //what is the value of doubleNum?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Array Destructuring
+
+// // let fellows = ['Aldo', 'Matt', 'Mohammad', 'Jarae', 'Andrew', 'Shawn']
+
+// // let [x, y, ...z] = fellows
+
+
+// // let person = { name: "Ann", age: 29, pet: "Zuko", height: 165 }
+
+// // let { age, pet, ...rest } = person
+// // // let age = ???
+// // // let pet = ???
+
+
+
+
+
+
+
+
+
+
+
+
+// // let array = [4, 5, 6]
+
+// // function add(param1, param2, ...rest){
+// //     //let rest = [ element are our arguements ]
+// //     console.log("param1 is", param1)
+// //     console.log("param2 is", param2)
+// //     for(let i = 0; i < rest.length; i++){
+// //         console.log(rest[i])
+// //     }
+// // }
