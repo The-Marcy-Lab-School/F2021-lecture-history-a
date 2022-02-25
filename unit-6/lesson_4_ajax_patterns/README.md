0e0fe0cb0285fc2067025ac0e31724dfb27041d0
Ann Duong <thuyanannduong@gmail.com>
2022-02-25 11:06:21 -0500

    async/await

--- unit-6/lesson_4_ajax_patterns/README.md
+++ unit-6/lesson_4_ajax_patterns/README.md
@@ -2,10 +2,26 @@
 
 ## Key Terms
 * `fetch`
-* JSON
-* Async/Await
-* Optimistic Rendering
-* Pessimistic Rendering
+* JSON (JavaScript Object Notation)
+* .json() vs .text()
+
+- GET: getting/receiving data
+- POST: sending/giving data 
+
+
+# Optimistic Rendering  (positive)
+* When I make a fetch (I'm very sure I'll get a response)
+* update your DOM before you get the response
+* faster DOM updates => happy user
+* Simulate a working app 
+* Gaming 
+
+# Pessimistic Rendering (doubtful)
+* When I make a fetch call (I'm not sure I'll get a response)
+* update your DOM after the response is given to you
+* What to make SURE the reponse was succesful so that we don't lie to our users  
+
+
 
 ## Essential Questions
 * How does asynchronously programming and the ability to make AJAX requests impact our applications?
--- unit-6/lesson_4_ajax_patterns/index.js
+++ unit-6/lesson_4_ajax_patterns/index.js
@@ -1,1 +1,68 @@
-console.log("hello world!")
+const token = "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"
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("fun-fact-button").addEventListener("click", getFact)
+  document.getElementById("dog-button").addEventListener("click", getDog)
+  document.getElementById("post-form").addEventListener("submit", postRepo)
+})
+
+async function getFact(){
+  let response = await fetch("http://numbersapi.com/10") //returns a promise to get a response
+  let string = await response.text()
+
+  const funFactDiv = document.getElementById("fun-fact-div");
+  funFactDiv.innerText = string;
+
+  // fetch("http://numbersapi.com/10")    
+  // .then(res => res.text())                            
+  // .then(string => {
+  //   const funFactDiv = document.getElementById("fun-fact-div");
+  //   funFactDiv.innerText = string;
+  // })
+}
+
+async function getDog(){
+  let response = await fetch("https://dog.ceo/api/breeds/image/random")
+  let obj = await response.json()
+
+  const dogDiv = document.getElementById("dog-div");
+  dogDiv.innerHTML = `<img src="${obj.message}" style="width: 300px;"/>`;
+
+  // fetch("https://dog.ceo/api/breeds/image/random")    
+  // .then(res => res.json())                            
+  // .then(data => {
+  //   const dogDiv = document.getElementById("dog-div");
+  //   dogDiv.innerHTML = `<img src="${data.message}" style="width: 300px;"/>`;
+  // })
+}
+
+function postRepo(e){
+  e.preventDefault();
+  const repoName = document.getElementById("post-input").value
+  
+  let optionsObject = {
+    method: "POST",
+    headers: { 
+      'Content-Type' : 'application/json',
+      'Authorization': `token ${token}`
+    },
+    body: JSON.stringify({
+      name: repoName,
+      description: "blah blah blah"
+    })
+  }
+  
+  const newRepoDiv = document.getElementById("post-div");
+  newRepoDiv.innerText = "Loading..."
+
+  fetch("https://api.github.com/user/repos", optionsObject)
+    .then(response => response.json())
+    .then(data => {
+      if(!data.name){
+        throw new Error()
+      }
+      newRepoDiv.innerText = `${data.name} was successfully created`;
+    }).catch(err => {
+      alert("Cound not create repo")
+    })
+}
